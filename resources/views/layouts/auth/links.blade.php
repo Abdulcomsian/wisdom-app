8d57fa1cb036bed4dc23f45e025d0221a68a2e4b
Abid Ali <abid.ali@accrualhub.com>
2025-03-22 14:38:07 +0500

    sdskdsad

--- resources/views/layouts/auth/links.blade.php
+++ resources/views/layouts/auth/links.blade.php
@@ -7,15 +7,11 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ ucfirst(auth()->user()->roles[0]->name) }} {{ $pageTitle != null ? '- ' . $pageTitle : '' }}</title>
 
-    {{-- @vite(['resources/assets/sass/app.scss', 'resources/assets/js/app.js']) --}}
-    {{-- @vite(["resources/css/app.css", "resources/js/app.js"]) --}}
-    {{-- @vite(['resources/scss/app.scss', 'resources/js/app.js']) --}}
     @vite(['resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tw itter-bootstrap/4.0.0-alpha/css/bootstrap.min.css">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-
-     alpha/css/bootstrap.css" rel="stylesheet">
-	<link rel="stylesheet" type="text/css"
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">	<link rel="stylesheet" type="text/css"
      href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
      <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
@@ -25,9 +21,9 @@
 </head>
 
 <body class="antialiased">
-    <div class="splash active">
+    {{-- <div class="splash active">
         <div class="splash-icon"></div>
-    </div>
+    </div> --}}
 
     <!-- Page Wrapper -->
     <div class="wrapper">
--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js'],
+            input: ['resources/js/app.js'], // Only one entry point now
             refresh: true,
         }),
     ],
     server: {
         watch: {
-            usePolling: false, // Reduce CPU usage
+            usePolling: false,
         },
     },
 });
